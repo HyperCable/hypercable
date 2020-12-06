f7ca7d90a6aba881fb715f0c4a4a4b15afc233e2
Hooopo Wang <hoooopo@gmail.com>
2020-12-06 21:59:25 +0800

    fix env docker example

--- db/hyper_schema.rb
+++ db/hyper_schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 2020_11_16_103826) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "timescaledb"
 
   create_table "worlds", force: :cascade do |t|
     t.string "name"
