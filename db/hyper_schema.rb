fd2ce8f9e65901450fda8eac1f27ad7ee79c2b95
Hooopo Wang <hoooopo@gmail.com>
2020-12-09 04:20:08 +0800

    setup hypertable

--- db/hyper_schema.rb
+++ db/hyper_schema.rb
@@ -10,11 +10,49 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_16_103826) do
+ActiveRecord::Schema.define(version: 2020_12_08_173343) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "timescaledb"
+
+  create_table "events", id: false, force: :cascade do |t|
+    t.string "visit_token"
+    t.string "user_id"
+    t.string "name"
+    t.jsonb "properties"
+    t.datetime "time", null: false
+    t.index ["time"], name: "events_time_idx", order: :desc
+  end
+
+  create_table "visits", id: false, force: :cascade do |t|
+    t.string "visit_token"
+    t.string "visitor_token"
+    t.string "visitor_id"
+    t.string "ip"
+    t.text "user_agent"
+    t.text "referrer"
+    t.string "referring_domain"
+    t.text "landing_page"
+    t.string "browser"
+    t.string "os"
+    t.string "device_type"
+    t.string "country"
+    t.string "region"
+    t.string "city"
+    t.float "latitude"
+    t.float "longitude"
+    t.string "utm_source"
+    t.string "utm_medium"
+    t.string "utm_term"
+    t.string "utm_content"
+    t.string "utm_campaign"
+    t.string "app_version"
+    t.string "os_version"
+    t.string "platform"
+    t.datetime "started_at", null: false
+    t.index ["started_at"], name: "visits_started_at_idx", order: :desc
+  end
 
   create_table "worlds", force: :cascade do |t|
     t.string "name"
