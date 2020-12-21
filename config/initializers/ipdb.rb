ce17b27064f34545ad407e03519c22e32334b384
Hooopo Wang <hoooopo@gmail.com>
2020-12-21 14:48:40 +0800

    fix ci

--- config/initializers/ipdb.rb
+++ config/initializers/ipdb.rb
@@ -1,3 +1,7 @@
 # frozen_string_literal: true
 
-IPDB = GeoIP2Compat.new("/usr/local/var/GeoIP/GeoLite2-City.mmdb")
+begin
+  IPDB = GeoIP2Compat.new("/usr/local/var/GeoIP/GeoLite2-City.mmdb")
+rescue GeoIP2Compat::Error
+  puts "run 'geoipupdate -d /usr/local/var/GeoIP' locally."
+end
