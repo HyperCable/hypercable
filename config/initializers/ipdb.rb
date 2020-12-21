bd07ce262eb51705fd63fcd75a7fd4eb9d296c42
Hooopo Wang <hoooopo@gmail.com>
2020-12-21 16:54:56 +0800

    add lur cache for mmdb

--- config/initializers/ipdb.rb
+++ config/initializers/ipdb.rb
@@ -1,7 +1,24 @@
 # frozen_string_literal: true
 
-begin
-  IPDB = GeoIP2Compat.new("/usr/local/var/GeoIP/GeoLite2-City.mmdb")
-rescue GeoIP2Compat::Error
-  puts "run 'geoipupdate -d /usr/local/var/GeoIP' locally."
+class IPDB
+  CACHE = LruRedux::Cache.new(10000)
+  begin
+    MMDB = GeoIP2Compat.new("/usr/local/var/GeoIP/GeoLite2-City.mmdb")
+  rescue GeoIP2Compat::Error
+    puts "run 'geoipupdate -d /usr/local/var/GeoIP' locally."
+  end
+
+  def self.get_from_mmdb(ip)
+    MMDB.lookup(ip) || {}
+  rescue GeoIP2Compat::Error
+    {}
+  end
+
+  def self.get(ip, cache = true)
+    if cache
+      CACHE.getset(ip) { self.get_from_mmdb(ip) }
+    else
+      self.get_from_mmdb(ip)
+    end
+  end
 end
