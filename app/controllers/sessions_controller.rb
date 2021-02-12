f7ae3d8404ddef3a92dda6c731d7d29cbdaef489
Hooopo Wang <hoooopo@gmail.com>
2021-02-12 17:35:44 +0800

    fix rubocop

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,7 +14,7 @@ class SessionsController < ApplicationController
       set_current_user(@user, user_params[:remember_me])
 
       if @user.email_verified?
-        flash[:success] = 'Login successfully'
+        flash[:success] = "Login successfully"
         redirect_to new_site_path
       else
         redirect_to verification_registrations_path, error: "Please verify your email"
